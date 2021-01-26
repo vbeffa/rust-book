8ce84f0a6884d1135ede0a5d7d561281b155342e
Vlad Beffa <vbeffa@mac.com>
2021-01-25 23:12:43 -0600

    functions, remove .gitignore

---
+++ functions/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5, 6);
+
+    expressions();
+
+    let x = five();
+
+    println!("The value of x is: {}", x);
+
+    let y = plus_one(five());
+
+    println!("The value of y is: {}", y);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("Another function.");
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn expressions() {
+    let x = 5;
+
+    println!("The value of x is: {}", x);
+
+    let y = {
+	let x = 3;
+	x + 1
+    };
+
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(i: i32) -> i32 {
+    i + 1
+}
