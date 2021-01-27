a9e230af0f19fbb9aa3a9bb0b345c0d9af71f96a
Vlad Beffa <vbeffa@mac.com>
2021-01-27 11:21:14 -0600

    control_flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,96 @@
+fn main() {
+    let number = 7;
+
+    if number < 5 {
+	println!("condition was true");
+    } else {
+	println!("condition was false");
+    }
+
+    six();
+
+    if_let();
+
+    do_loop();
+
+    do_while();
+
+    index_while();
+
+    loop_for();
+
+    for_range();
+}
+
+fn six() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
+
+fn if_let() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of the number is: {}", number);
+}
+
+fn do_loop() {
+    let mut counter = 0;
+    
+    let result = loop {
+	counter += 1;
+	
+	if counter == 10 {
+	    break counter * 2;
+	}
+    };
+
+    println!("The result is: {}", result);
+}
+
+fn do_while() {
+    let mut number = 3;
+
+    while number != 0 {
+	println!("{}!", number);
+
+	number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn index_while() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+	println!("The value is: {}", a[index]);
+
+	index += 1;
+    }
+}
+
+fn loop_for() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+	println!("The value is: {}", element);
+    }
+}
+
+fn for_range() {
+    for number in (1..4).rev() {
+	println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
